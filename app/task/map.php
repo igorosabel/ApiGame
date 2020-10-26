92f68c357d61075aa849bb6fefe1322a7ac5e9b1
Iñigo <inigo.gorosabel@gmail.com>
2020-10-26 10:42:19 +0100

    Correccion en la proporcion de los objetos al generar el mapa y actualizo el escenario Inicio

--- app/task/map.php
+++ app/task/map.php
@@ -3,7 +3,7 @@ class mapTask extends OTask {
 	public function __toString() {
 		return "map: Nueva tarea map";
 	}
-	
+
 	private $tile_size = 32;
 	private $rows = 20;
 	private $cols = 25;
@@ -26,6 +26,17 @@ class mapTask extends OTask {
 				return imagecreatefromgif($file);
 			}
 		}
+	}
+
+	private function calculatePosition(int $start_x, int $start_y, int $width, int $height): array {
+		$ret = [
+			'x' => ($start_x * $this->tile_size) - (($width-1) * $this->tile_size),
+			'y' => $start_y * $this->tile_size - (($height-1) * $this->tile_size),
+			'width' => $width * $this->tile_size,
+			'height' => $height * $this->tile_size
+		];
+
+		return $ret;
 	}
 
 	public function run(array $options=[]): void {
@@ -53,6 +64,7 @@ class mapTask extends OTask {
 		$outputImage = imagecreatetruecolor(($this->cols * $this->tile_size), ($this->rows * $this->tile_size));
 		$data = $scenario->getData();
 
+		// Background
 		foreach ($data as $scenario_data) {
 			$this->getLog()->debug("X: ".$scenario_data->get('x')." - Y: ".$scenario_data->get('y'));
 			$bg_file = $scenario_data->getBackground()->getAsset()->getFile();
@@ -61,14 +73,23 @@ class mapTask extends OTask {
 			$bg = $this->getResource($bg_file, $bg_file_ext);
 
 			imagecopyresized($outputImage, $bg, ($scenario_data->get('y') * $this->tile_size), ($scenario_data->get('x') * $this->tile_size), 0, 0, $this->tile_size, $this->tile_size, $width, $height);
+		}
 
+		// Scenario objects
+		foreach ($data as $scenario_data) {
 			if (!is_null($scenario_data->getScenarioObject())) {
 				$so_file = $scenario_data->getScenarioObject()->getAsset()->getFile();
 				$so_file_ext = $scenario_data->getScenarioObject()->getAsset()->get('ext');
 				list($width, $height) = getimagesize($so_file);
 				$so = $this->getResource($so_file, $so_file_ext);
+				$so_position = $this->calculatePosition(
+					$scenario_data->get('y'),
+					$scenario_data->get('x'),
+					$scenario_data->getScenarioObject()->get('width'),
+					$scenario_data->getScenarioObject()->get('height')
+				);
 
-				imagecopyresized($outputImage, $so, ($scenario_data->get('y') * $this->tile_size), ($scenario_data->get('x') * $this->tile_size), 0, 0, $this->tile_size, $this->tile_size, $width, $height);
+				imagecopyresized($outputImage, $so, $so_position['x'], $so_position['y'], 0, 0, $so_position['width'], $so_position['height'], $width, $height);
 			}
 		}
 
